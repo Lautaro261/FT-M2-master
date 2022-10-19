8adb68224554a4191d9bda220f2704365e14d277
Lautaro261 <nahuelygoofy@gmail.com>
2022-10-19 03:18:03 -0300

    o

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import s from './Card.Module.css';
 
-export default function Card(props) {
+export default function Card({name, min, max, img, onclose}) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+   <div className={s.contenedor}>
+      <button onClick={onclose} className={s.btn}>X</button>
+      <h4>{name}</h4>
+    <div className={s.infoCont}>
+      <div>
+        <p>Min: {min}°</p>
+      </div>
+      <div>
+        <p>Max: {max}°</p>
+      </div>
+
+      </div>
+      <img src= {`http://openweathermap.org/img/wn/${img}@2x.png`} alt='found not defi'/>
+
+    </div>)
+
+
 };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import s from './SearchBar.Module.css';
+
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div className={s.btn}>
+      <input type="text" placeholder='🔍Cuidad...' />
+      <button onClick={() => props.onSearch('Buscando...')}>Agregar</button>
+    </div>
+    )
 };
